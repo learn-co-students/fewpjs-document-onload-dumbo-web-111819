159f7df410525b2c9827261c3e4754bed2e299c6
MohammedQasim <mqshaikh8@gmail.com>
2020-01-06 17:05:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    editText()
+  });
+  function editText(){
+    document.getElementById( "text" ).innerHTML = "This is really cool!";
+  }
