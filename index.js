c0ea7650a601eee105f42f530373cb54211f0050
Baruch Phillips <baruchphillips@gmail.com>
2020-01-06 16:36:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+// let variable = document.getElementById("text")
+// variable.
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('text').innerHTML = "This is really cool!"
+  });
+  
